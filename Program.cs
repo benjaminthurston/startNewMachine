018ab74311c0539a167f01fc41e483201ae5848a
benjaminthurston <benjamin.thurston87@gmail.com>
2020-05-26 19:20:43 -0400

    adding the C# parts

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace startNewMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
